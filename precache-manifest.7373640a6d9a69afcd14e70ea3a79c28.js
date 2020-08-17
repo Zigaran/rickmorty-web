905018c28783fb6a2f71f88d6424c552eba4b185
Zigaran <zigaraneliasnatan@gmail.com>
2020-08-17 19:06:11 -0300

    Updates

---
+++ precache-manifest.7373640a6d9a69afcd14e70ea3a79c28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0df02a2cc14af9f3300aeafe360e734",
+    "url": "/rickmorty-web/index.html"
+  },
+  {
+    "revision": "35000d461caf66d949de",
+    "url": "/rickmorty-web/static/css/2.2d39c47b.chunk.css"
+  },
+  {
+    "revision": "c246bbf3d4aca2cbe249",
+    "url": "/rickmorty-web/static/css/main.2b7e26e1.chunk.css"
+  },
+  {
+    "revision": "35000d461caf66d949de",
+    "url": "/rickmorty-web/static/js/2.6a1700ab.chunk.js"
+  },
+  {
+    "revision": "f863024c2d9d2f2bdd781cf3f534e630",
+    "url": "/rickmorty-web/static/js/2.6a1700ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c246bbf3d4aca2cbe249",
+    "url": "/rickmorty-web/static/js/main.6d497fb8.chunk.js"
+  },
+  {
+    "revision": "bc3f5e3652d2560f32c5",
+    "url": "/rickmorty-web/static/js/runtime-main.55b32f20.js"
+  }
+]);
